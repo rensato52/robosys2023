9b962b8532cee5aaae7c1e188240e521d9b8b63d
Ren Sato <miseria.ren@gmail.com>
2023-10-26 12:57:31 +0900

    Add test human support

--- test.bash
+++ test.bash
@@ -1,4 +1,16 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Ren Sato
+# SPDX-LIcense-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
 
