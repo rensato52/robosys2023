df39097ae971cd6c74cbdd95ec24c38436ebe5eb
Ren Sato <miseria.ren@gmail.com>
2023-10-26 13:05:22 +0900

    Add test cases

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Ren Sato
 # SPDX-LIcense-Identifier: BSD-3-Clause
 
@@ -10,6 +10,15 @@ ng () {
 res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
