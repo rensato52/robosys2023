d8dd0391197e8e33f699b5d929cbadb00ff32f5e
Ren Sato <miseria.ren@gmail.com>
2023-11-24 14:09:51 +0900

    Add testcase

---
+++ test.bash
@@ -0,0 +1,25 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Ren Sato
+# SPDX-LIcense-Identifier: BSD-3-Clause
+
+ng () {
+        echo NG at Line $1
+        res=1
+}
+
+res=0
+out=$(seq 5 | ./calculation)
+[ "${out}" = "和:15, 差:-15, 積:120, 商:0.008333333333333333, 平均値:3.0" ] || ng ${LINENO}
+
+out=$(echo あ | ./calculation)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo  | ./calculation)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
