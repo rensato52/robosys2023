4229a0a920643e49e7aa6412e32ace2a93d92db9
Ren Sato <miseria.ren@gmail.com>
2023-11-28 14:29:52 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Ren Sato
 # SPDX-LIcense-Identifier: BSD-3-Clause
 
@@ -9,9 +9,12 @@ ng () {
 
 
 res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = "15 -15 120 0.008333333333333333 3.0" ] || ng ${LINENO}
 
+### STRANGE INPUT ###
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
